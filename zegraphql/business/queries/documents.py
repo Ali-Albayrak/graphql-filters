cb7c93b219319689bcdbeecae80db006e8f37f52
Ali Albayrak <l1120910@std.yildiz.edu.tr>
2024-09-24 15:27:55 +0300

    add filters and pagination
    Changes:
    - Create new strawberry input with name `QuerySchema` to handle filters and pagination
    - Create `FieldFilter` input to contain fields that will be filtered based on operators
    - Create `QueryOperator` input that contain allowed operations
    - Create new Read type for each model to have list of the model and pagination vars (`page_size`, `next_page`)
    - Update query resolvers to use new `QuerySchema` and return paginated result
    --------------------------------------
    Update Manger Class:
    - update `_query` to hold all required info to build query (limit, offset, sort, filters_list, filters_dict)
    - Add new `_build_filters_list` function to create `BinaryExpression` objs that can be used in filtering
    - Add new `_build_sort` function to create `UnaryExpression` that can be used to sort query
    - update `__fetch`, `all`, and `get` functions to update new `_query` structure
    - Add type annotations to mark manger class as generic class
    --------------------------------------
    Fixes & adaptations:
    - Fix auto camel case
    - Adapt `get_multiple` (test it)

--- zegraphql/business/queries/documents.py
+++ zegraphql/business/queries/documents.py
@@ -1,10 +1,12 @@
+import uuid
 import strawberry
 from strawberry.permission import PermissionExtension
 from fastapi import HTTPException
-from business.types import DocumentType
+from business.types import DocumentType, ReadDocuments
 from business.db_models.documents_model import DocumentModel, DocumentsAccess
 from core.constants import AppConstants as AC
 from core.depends import GraphQLContext
+from core.filter_type import QuerySchema
 from core.auth import Protect
 from core import log
 
@@ -15,7 +17,7 @@ class DocumentQuery:
         db = info.context.db
         try:
             obj = DocumentModel.objects(db)
-            result = await obj.get(id=id)
+            result = await obj.get(id=uuid.UUID(id))
             return result
         except Exception as e:
             log.debug(AC.ERROR_TEMPLATE.format(f"get_document with id {id}", type(e), str(e)))
@@ -26,8 +28,11 @@ class DocumentQuery:
         db = info.context.db
         try:
             obj = DocumentModel.objects(db)
-            result = await obj.all()
-            return result
+            offset = (q.page-1)*q.page_size if q.page_size > 0 else 0
+            result = await obj.all(offset=offset, limit=q.page_size, sort=q.sort, filters=q.filters)
+            # log.debug(f"Result: {result[0].id}, Count: {len(result)}")
+            # return ReadDocuments(documents=result, page_size=q.limit, next_page=q.skip+q.limit if len(result) == q.limit else None)
+            return ReadDocuments(documents=result, page_size=q.page_size, next_page=q.page+1 if len(result) == q.page_size else None)
         except Exception as e:
             log.debug(AC.ERROR_TEMPLATE.format(f"list_documents", type(e), str(e)))
             raise HTTPException(500, f"failed to fetch documents")
